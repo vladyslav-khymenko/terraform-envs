a398103ee9344dd9a1706ef7497938abedd72ea8
vladyslav-khymenko <vkhymenko@protonmail.com>
2023-04-10 20:39:52 +0300

    used for_each loop to create multiple iam users

--- global/iam/main.tf
+++ global/iam/main.tf
@@ -3,7 +3,12 @@ provider "aws" {
   profile = "vladyslav.khymenko-user"
 }
 
+# resource "aws_iam_user" "example" {
+#   count = length(var.user_names)
+#   name  = var.user_names[count.index]
+# }
+
 resource "aws_iam_user" "example" {
-  count = length(var.user_names)
-  name  = var.user_names[count.index]
+  for_each = toset(var.user_names)
+  name     = each.value
 }
--- global/iam/outputs.tf
+++ global/iam/outputs.tf
@@ -1,9 +1,3 @@
-output "first_arn" {
-  value       = aws_iam_user.example[0].arn
-  description = "The ARN for the first user"
-}
-
-output "all_arns" {
-  value       = aws_iam_user.example[*].arn
-  description = "The ARNs for all users"
+output "all_users" {
+  value = aws_iam_user.example
 }
