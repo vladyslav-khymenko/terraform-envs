06c6778debe37298a5fdcfed49e3d5828b4c6786
vladyslav-khymenko <vkhymenko@protonmail.com>
2023-04-30 22:49:55 +0300

    added required_providers, updated url to the module

--- stage/services/web-server-cluster/main.tf
+++ stage/services/web-server-cluster/main.tf
@@ -7,7 +7,7 @@ terraform {
 }
 
 module "web_server_cluster" {
-  source = "github.com/vladyslav-khymenko/terraform-modules//services/web-server-cluster?ref=v0.0.10"
+  source = "github.com/vladyslav-khymenko/terraform-modules//services/web-server-cluster?ref=v0.0.11"
 
   ami         = "ami-0fb653ca2d3203ac1"
   server_text = "New server text from stage"
